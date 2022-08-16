9c2eeb886f3888e9515040deedaf4704e20dcf6f
Isaiah Keller <isaiah.keller@gmail.com>
2022-08-16 16:37:21 -0700

    displaying images and counter working

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import Button from './Button'
 
 class HornedBeast extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
+
+        this.state = {
+            likes: 0
+        }
+
+    }
+
+    incriment = (e) => {
+        console.log('inc')
+        let nextState = {
+            likes: this.state.likes + 1
+        };
+        this.setState(nextState)
+        console.log(this.state.likes)
+    }
+
+    decriment = (e) => {
+        console.log('dec')
+        let nextState = {
+            likes: this.state.likes - 1
+        };
+        this.setState(nextState)
+        console.log(this.state.likes)
     }
 
     render() {
-    return (
-        <div className='horned-beast'>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.imageUrl} alt={this.props.title} />
-            <p>{this.props.description}</p>
-        </div>
-    )}
+        return (
+            <div className='horned-beast'>
+                <h2>{this.props.title}</h2>
+                <img src={this.props.image} alt={this.props.title} />
+                <Button prompt='+' handleClick={this.incriment} />
+                {`❤${this.state.likes}`}
+                <Button prompt='-' handleClick={this.decriment} />
+                <p>{this.props.description}</p>
+            </div>
+        )
+    }
 }
 
 export default HornedBeast;
