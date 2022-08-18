12610f1329329175319540ac31f70121c256f0f0
Isaiah Keller <isaiah.keller@gmail.com>
2022-08-17 20:58:37 -0700

    got the modal to show up but I'm having trouble with putting the selected beast into the modal

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
-import './App.css'
+import BeastsArray from './data.json';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main BeastsArray={BeastsArray} />
       <Footer />
     </div>
   );
